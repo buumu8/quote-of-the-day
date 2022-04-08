c3264cfb999b56324a0c4255e57ac0e728512b73
buumu8 <buumu8@gmail.com>
2022-04-08 18:41:28 +0700

    add netlify-lambda for express

--- server/express/server.js
+++ server/express/server.js
@@ -1,12 +1,24 @@
+"use strict";
 const axios = require("axios");
 const express = require("express");
+const serverless = require("serverless-http");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const quotes = require("./quotes.js");
 const app = express();
 
+const router = express.Router();
+router.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Hello from Express.js!</h1>");
+  res.end();
+});
+router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
+router.post("/", (req, res) => res.json({ postBody: req.body }));
+
 app.use(cors());
 app.use(bodyParser.json());
+app.use("/.netlify/functions/server", router); // path must route to lambda
 
 app.get("/", function (req, res) {
   res.send("Hello World");
@@ -26,6 +38,9 @@ app.post("/wiki", (req, res) => {
     .catch((e) => res.status(400).json(e));
 });
 
-app.listen(3002, function () {
-  console.log("CORS-enabled web server listening on port 3002");
-});
+// app.listen(3002, function () {
+//   console.log("CORS-enabled web server listening on port 3002");
+// });
+
+module.exports = app;
+module.exports.handler = serverless(app);
