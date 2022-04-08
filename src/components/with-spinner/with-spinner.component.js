5a061d4e1b4e3df7b14da87f27256c98429b6f7a
buumu8 <buumu8@gmail.com>
2022-04-08 11:23:32 +0700

    delete components folder

--- src/components/with-spinner/with-spinner.component.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-import { Spinner } from "../spinner/spinner.component";
-
-export const WithSpinner =
-  (WrappedComponent) =>
-  ({ isLoading, ...otherProps }) =>
-    isLoading ? <Spinner /> : <WrappedComponent {...otherProps} />;
