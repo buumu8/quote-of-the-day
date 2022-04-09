6cc4efac1fd00d26d8888d137d8276c408776384
buumu8 <buumu8@gmail.com>
2022-04-09 08:52:15 +0700

    test wiki serverless functions

--- netlify/functions/wiki.js
+++ netlify/functions/wiki.js
@@ -2,21 +2,23 @@ const axios = require("axios");
 
 exports.handler = async function (event, context) {
   const { author } = JSON.parse(event.body);
-  axios
-    .get(
-      `http://en.wikipedia.org/w/api.php?action=query&titles=${author}&prop=pageimages|info&inprop=url&format=json&pithumbsize=100`
-    )
-    .then((result) => {
-      const data = result.data;
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message: data }),
-      };
-    })
-    .catch((e) => {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ message: "error: " + e }),
-      };
-    });
+  const API_ENDPOINT = `http://en.wikipedia.org/w/api.php?action=query&titles=${author}&prop=pageimages|info&inprop=url&format=json&pithumbsize=100`;
+  let response;
+
+  try {
+    const result = await axios.get(API_ENDPOINT);
+    response = result.data;
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({ message: "error: " + err }),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: response,
+    }),
+  };
 };
